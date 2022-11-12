452b0bf9f087d642200d8441e3af550ad7ed60b0
juanjoselozano <jjuanlozanochoa1993@gmail.com>
2022-11-12 05:13:13 -0500

    update

---
+++ week8/script.js
@@ -0,0 +1,47 @@
+const fill = document.querySelector('.fill-box');
+const empties = document.querySelectorAll('.emptyBox');
+
+// Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empty boxes and add listeners
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+
+function dragStart() {
+  this.className += ' hold';
+  setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+  this.className = 'fill-box';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
+}
+
+function dragLeave() {
+  this.className = 'emptyBox';
+}
+
+function dragDrop() {
+  this.className = 'emptyBox';
+  this.append(fill);
+}
+
+
+
+
